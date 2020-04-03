b5295440f699641511f02901432a9cc5ef7b26ea
Artem Cvetkov <artemcv88@gmail.com>
2020-04-03 14:29:21 +0400

    w

--- testGitProj/Form1.Designer.cs
+++ testGitProj/Form1.Designer.cs
@@ -33,7 +33,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(173, 382);
+            this.button1.Location = new System.Drawing.Point(196, 444);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
