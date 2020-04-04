847344bf64bfd783454a614825a16d5de74abcf6
Oleg Privalov <galva@mail.ru>
2020-04-04 15:02:55 +0300

    Update 5

--- testGitProj/Form1.Designer.cs
+++ testGitProj/Form1.Designer.cs
@@ -53,7 +53,7 @@
             this.chart1.ChartAreas.Add(chartArea1);
             legend1.Name = "Legend1";
             this.chart1.Legends.Add(legend1);
-            this.chart1.Location = new System.Drawing.Point(27, 41);
+            this.chart1.Location = new System.Drawing.Point(190, 55);
             this.chart1.Name = "chart1";
             series1.ChartArea = "ChartArea1";
             series1.Legend = "Legend1";
